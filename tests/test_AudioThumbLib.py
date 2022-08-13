014762e4ec132e462cb1680b3313ee22d0222a61
yrammos <y@rammos.co>
2022-08-14 00:19:33 +0200

    Update test.

--- tests/test_AudioThumbLib.py
+++ tests/test_AudioThumbLib.py
@@ -12,23 +12,24 @@ def test_one():
     print(json.dumps(t.thumbnail, indent=2))
 
     assert t.thumbnail == {
-        "filename": "Monk.mp3",
-        "thumbnail": {
-            "boundaries_in_seconds": "[[20.0, 50.0], [145.5, 172.5]]",
-            "fitness": "0.135",
-            "nominal_duration_in_seconds": 30,
-            "search_step_in_seconds": 5,
-            "total_coverage_in_seconds": 58.0,
-            "normalized_total_coverage": "0.152",
-            "score": "74.747",
-            "normalized_score": "0.122"
-        },
-        "context": {
-            "audio_duration_seconds": "181.16",
-            "feature_rate_hz": 2.0,
-            "ssm_dimensions": {
-                "x": 363,
-                "y": 363
+            "filename": "Monk.mp3",
+            "thumbnail": {
+                "boundaries_in_seconds": "[[15.0, 29.0], [33.0, 49.0], [49.5, 61.0], [72.5, 89.5], [155.0, 172.0]]",
+                "fitness": "0.354",
+                "nominal_duration_in_seconds": 17,
+                "search_step_in_seconds": 5,
+                "thumbnail_duration_step_in_seconds": 1,
+                "coverage_in_seconds": 78.0,
+                "normalized_coverage": "0.333",
+                "score": "94.002",
+                "normalized_score": "0.378"
+            },
+            "context": {
+                "audio_duration_in_seconds": "181.16",
+                "feature_rate": 2.0,
+                "ssm_dimensions": {
+                    "x": 363,
+                    "y": 363
+                }
             }
         }
-    }
