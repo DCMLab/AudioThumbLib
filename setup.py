a156b1408e8d50bc85ed9cc26b78a2b27214bc7d
yrammos <y@rammos.co>
2022-08-14 02:06:36 +0200

    Package for pip.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='lunch_options',
+      version='0.1',
+      description='A CLI tool and Python module for audio file thumbnailing.',
+      url='https://github.com/DCMLab/AudioThumbLib',
+      packages=['audiothumblib'],
+)
